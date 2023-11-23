d31ae3ab39a1669e5258da6620a2cc3e88091b8e
Charles787205 <j.canedo.527294@umindanao.edu.ph>
2023-11-23 23:04:54 +0800

    update

--- patient/components/header.php
+++ patient/components/header.php
@@ -147,7 +147,7 @@ $user = $userModel->getUserById($user_id);
           </li>
 
           <li>
-            <a class="dropdown-item d-flex align-items-center" href="#">
+            <a class="dropdown-item d-flex align-items-center" href="sign-out.php">
               <i class="bi bi-box-arrow-right"></i>
               <span>Sign Out</span>
             </a>
